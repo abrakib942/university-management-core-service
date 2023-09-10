915a1e84566485c7297120e68e88039c7a7327e0
A.B. Rakib <96842069+abrakib942@users.noreply.github.com>
2023-09-10 20:15:55 +0600

    course

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,13 +1,48 @@
 import express from 'express';
+import { academicDepartmentRoutes } from '../modules/academicDepartment/academicDepartment.routes';
+import { academicFacultyRoutes } from '../modules/academicFaculty/academicFaculty.routes';
 import { AcademicSemesterRoutes } from '../modules/academicSemester/academicSemester.routes';
+import { buildingRoutes } from '../modules/building/building.routes';
+import { courseRoutes } from '../modules/course/course.routes';
+import { facultyRoutes } from '../modules/faculty/faculty.routes';
+import { roomRoutes } from '../modules/room/room.routes';
+import { studentRoutes } from '../modules/student/student.routes';
 
 const router = express.Router();
 
 const moduleRoutes = [
   // ... routes
   {
-    path: '/academic-semester',
+    path: '/academic-semesters',
     route: AcademicSemesterRoutes,
+  },
+  {
+    path: '/academic-faculties',
+    route: academicFacultyRoutes,
+  },
+  {
+    path: '/academic-departments',
+    route: academicDepartmentRoutes,
+  },
+  {
+    path: '/faculties',
+    route: facultyRoutes,
+  },
+  {
+    path: '/students',
+    route: studentRoutes,
+  },
+  {
+    path: '/buildings',
+    route: buildingRoutes,
+  },
+  {
+    path: '/rooms',
+    route: roomRoutes,
+  },
+  {
+    path: '/courses',
+    route: courseRoutes,
   },
 ];
 
