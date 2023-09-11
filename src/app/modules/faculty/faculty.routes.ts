db7c0b8e0731f962cfe333b1430534e7eb2cb5a4
A.B. Rakib <96842069+abrakib942@users.noreply.github.com>
2023-09-11 11:37:18 +0600

    faculty

--- src/app/modules/faculty/faculty.routes.ts
+++ src/app/modules/faculty/faculty.routes.ts
@@ -11,24 +11,37 @@ router.get('/', FacultyController.getAllFromDB);
 
 router.get('/:id', FacultyController.getByIdFromDB);
 
-
 router.post(
-    '/',
-    validateRequest(FacultyValidation.create),
-    FacultyController.insertIntoDB
+  '/',
+  validateRequest(FacultyValidation.create),
+  FacultyController.insertIntoDB
 );
 
 router.patch(
-    '/:id',
-    validateRequest(FacultyValidation.update),
-    auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
-    FacultyController.updateOneInDB
+  '/:id',
+  validateRequest(FacultyValidation.update),
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  FacultyController.updateOneInDB
 );
 
 router.delete(
-    '/:id',
-    auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
-    FacultyController.deleteByIdFromDB
+  '/:id',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  FacultyController.deleteByIdFromDB
+);
+
+router.post(
+  '/:id/assign-courses',
+  validateRequest(FacultyValidation.assignOrRemoveCourses),
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  FacultyController.assignCourses
+);
+
+router.delete(
+  '/:id/remove-courses',
+  validateRequest(FacultyValidation.assignOrRemoveCourses),
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  FacultyController.removeCourses
 );
 
 export const facultyRoutes = router;
